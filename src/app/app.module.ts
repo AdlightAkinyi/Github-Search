42c6a19cf4fdfa03bcf1a76efe33b698ff126b43
C0159412020 <adlightakinyi1@gmail.com>
2022-04-19 12:18:13 +0300

    added imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { HighlightDirective } from './highlight.directive';
+import { BoldenPipe } from './bolden.pipe';
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HighlightDirective,
+    BoldenPipe,
+    
+
+         
+      
+    
+    
+    
   ],
-  imports: [
+  imports:[
     BrowserModule,
-    AppRoutingModule
+      FormsModule,
+      HttpClientModule,
+      
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+     
+      
+  
+  
+  
+   
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
